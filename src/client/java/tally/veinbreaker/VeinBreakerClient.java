59983990bcbf01e22d2ae485b79e5b707926096f
Yande <yande@yande.dev>
2025-04-29 11:32:19 -0400

    Initial commit

---
+++ src/client/java/tally/veinbreaker/VeinBreakerClient.java
@@ -0,0 +1,10 @@
+package tally.veinbreaker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeinBreakerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
